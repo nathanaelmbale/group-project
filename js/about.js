3af13aafd4bf9f895f5242504a01459b7716a76f
Nathanael Mbale <66083904+nathanaelmbale@users.noreply.github.com>
2023-11-27 12:09:40 +0300

    members loaded by XML

--- js/about.js
+++ js/about.js
@@ -13,7 +13,41 @@ $(document).ready(function () {
             console.error("Failed to load XML description");
         }
     });
+
+    $.ajax({
+        type: "GET",
+        url: "xml/team-members.xml", // Change to the correct path
+        dataType: "xml",
+        success: function (xml) {
+            displayTeamMembers(xml);
+        },
+        error: function () {
+            console.error("Failed to load XML team members");
+        }
+    });
+
 });
+
+
+
+function displayTeamMembers(xml) {
+    var wrapper = $(".wrapper");
+
+    $(xml).find("member").each(function () {
+        var card = $('<div class="card"></div>');
+        var imgSrc = $(this).find("image").text();
+        var name = $(this).find("name").text();
+        var role = $(this).find("role").text();
+
+        var img = $('<img src="' + imgSrc + '" alt="' + name + '">');
+        var body = $('<div class="body"><h3>' + name + '</h3><p>' + role + '</p></div>');
+
+        card.append(img);
+        card.append(body);
+
+        wrapper.append(card);
+    });
+}
 
 function navigateToExplore() {
     window.location.href = 'explore.html';
